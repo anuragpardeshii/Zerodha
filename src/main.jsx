607e7dd84530e5f7f8b026f7c2fef846a9d32d87
Anurag Pardeshi <pardeshianurag22@gmail.com>
2024-09-23 22:30:50 +0530

    Resources

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
+import HomePage from './landing_page/home/HomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
