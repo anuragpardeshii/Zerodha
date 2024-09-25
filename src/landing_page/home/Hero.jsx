ef494e2010d472da6e3a4e087eef38ad002e31aa
Anurag Pardeshi <pardeshianurag22@gmail.com>
2024-09-25 22:13:12 +0530

    completed home components

--- src/landing_page/home/Hero.jsx
+++ src/landing_page/home/Hero.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 function Hero() {
-    return ( 
-        <div>
-            
+    return (
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
+                <img src="media/Images/homeHero.png" alt="Hero Image" className='mb-5'/>
+                <h1>Invest in Evenrything</h1>
+                <p>Online platform to invest in stocks, derivaties, mutual funds and more</p>
+                <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:"0 auto"}}>SignUp Now</button>
+            </div>
         </div>
-     );
+    );
 }
 
 export default Hero;
