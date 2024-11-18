168d91f53ed1d6b4de824531c05f8db8fac9cc26
Anurag Pardeshi <pardeshianurag22@gmail.com>
2024-11-18 21:39:57 +0530

    commit

--- frontend/test/Hero.test.js
+++ frontend/test/Hero.test.js
@@ -3,6 +3,11 @@ import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import Hero from "../src/landing_page/home/Hero";
 
+// Mock the static file import if necessary
+jest.mock('../src/landing_page/home/Hero', () => () => (
+    <img src="media/Images/homeHero.png" alt="Hero Image" />
+));
+
 // Test Suite
 describe('Hero Component', () => {
     test("renders hero image", () => {
